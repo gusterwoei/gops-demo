97c8d00720ba04b8e1ec8ed482cfb69a404e9b79
inglab-guster <guster@inglab.com.my>
2020-07-05 21:22:04 +0800

    update app title

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Create Next App</title>
+                <title>Gops Demo</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
